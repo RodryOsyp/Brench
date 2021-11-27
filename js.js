8c58af91293781883af925782f04382637f2beb1
RodryOsyp <osypenkotymur@gmail.com>
2021-11-27 19:46:22 +0200

    srdy

---
+++ js.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $(".btn1").click(function() {
+			$("#image").attr("src", "https://raskrasiki.ru/wp-content/uploads/2020/04/bez-imeni-1-7.png")
+		});
+        $(".btn2").click(function() {
+            $("#image").attr("src", "https://static.wikia.nocookie.net/brawlstars/images/0/05/Starr_Poco.png/revision/latest/scale-to-width-down/250?cb=20200920133641&path-prefix=ru")
+		});
+
+
+})
